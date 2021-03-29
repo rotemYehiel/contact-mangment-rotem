94bbb4a0292e3d3d6fdad917d98f8332b0e250ea
Rotem Patrakeev <rotem8y@gmail.com>
2021-03-29 08:46:44 +0300

    base url user

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const baseUrl = 'https://contact-manager-rotem.herokuapp.com/api/user/';
-const baseUrl = 'https://localhost:8000/api/user/';
+const baseUrl = 'https://contact-manager-rotem.herokuapp.com/api/user/';
 
 
 const getUser = (credentials) => {
